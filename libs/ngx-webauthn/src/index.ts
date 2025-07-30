bd1b390592f13bc17f9f6206d23f4e164c53317e
Jonny Heavey <jonny@backbase.com>
2025-07-30 17:56:19 +0100

    remove clientside challenges

--- libs/ngx-webauthn/src/index.ts
+++ libs/ngx-webauthn/src/index.ts
@@ -61,7 +61,6 @@ export {
   base64ToArrayBuffer,
   stringToArrayBuffer,
   arrayBufferToString,
-  generateChallenge,
   generateUserId,
   credentialIdToArrayBuffer,
   arrayBufferToCredentialId,
