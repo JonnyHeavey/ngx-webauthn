88c4a8b70de46302355d363bcb0ecf5c84a42ae9
Jonny Heavey <jonnyheavey@gmail.com>
2025-12-27 19:27:43 +0000

    fix: reduce public api to intended symbols

--- libs/ngx-webauthn/src/index.ts
+++ libs/ngx-webauthn/src/index.ts
@@ -17,28 +17,17 @@ export { provideWebAuthn } from './lib/providers/webauthn.providers';
 
 // Core models and response types
 export type {
-  WebAuthnRegistrationResult,
-  WebAuthnAuthenticationResult,
-  WebAuthnSupport,
   RegistrationResponse,
   AuthenticationResponse,
   RegisterConfig,
   AuthenticateConfig,
-  RegisterInput,
-  AuthenticateInput,
   PresetName,
-  EnhancedRelyingParty,
-  FlexibleUserId,
-  FlexibleChallenge,
-  FlexibleCredentialDescriptors,
-  RemoteRegistrationRequest,
-  RemoteAuthenticationRequest,
+  WebAuthnSupport,
 } from './lib/model';
 
 // Error types for enhanced error handling
 export {
   WebAuthnError,
-  WebAuthnErrorType,
   UserCancelledError,
   AuthenticatorError,
   InvalidOptionsError,
@@ -50,46 +39,8 @@ export {
   InvalidRemoteOptionsError,
 } from './lib/errors/webauthn.errors';
 
-// Remote error context type
-export type { RemoteErrorContext } from './lib/errors/webauthn.errors';
-
-// Utility functions
-export {
-  arrayBufferToBase64url,
-  base64urlToArrayBuffer,
-  arrayBufferToBase64,
-  base64ToArrayBuffer,
-  stringToArrayBuffer,
-  arrayBufferToString,
-  generateUserId,
-  credentialIdToArrayBuffer,
-  arrayBufferToCredentialId,
-  isWebAuthnSupported,
-  isPlatformAuthenticatorAvailable,
-  getSupportedTransports,
-  validateRegistrationOptions,
-  getDefaultPubKeyCredParams,
-  isJSONOptions,
-  isPublicKeyCredential,
-} from './lib/utils/webauthn.utils';
-
-// Remote validation utilities
-export {
-  validateRemoteCreationOptions,
-  validateRemoteRequestOptions,
-} from './lib/utils/remote.utils';
-
-// Type guards
-export {
-  isRegisterConfig,
-  isAuthenticateConfig,
-  isCreationOptions,
-  isRequestOptions,
-} from './lib/utils/type-guards';
-
 // Preset configurations
 export {
-  PRESET_MAP,
   PASSKEY_PRESET,
   EXTERNAL_SECURITY_KEY_PRESET,
   PLATFORM_AUTHENTICATOR_PRESET,
